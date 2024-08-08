58d5f7ef7702e34170b16c8f83e2c81d96d7d15f
ayumu <ayu2d2@gmail.com>
2024-08-08 15:42:35 +0900

    test

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,6 +1,29 @@
+"use client"
+import { useCallback } from "react";
 import Link from 'next/link'
 export default function Page() {
+    const handleClick = useCallback(()=> {
+        fetch("https://pleasant-wahoo-48885.upstash.io/set/user_1_session/session_token_value", {
+            headers: {
+              Authorization: "Bearer Ab71AAIjcDE5ZDk0NTQ5MWY4ZGQ0Y2IxYTdkZTc5MGZkODIzZTQzZHAxMA"
+            }
+          }).then(response => response.json())
+            .then(data => console.log(data));
+
+    },[])
+ 
+    const handleGet = useCallback(()=> {
+        fetch("https://pleasant-wahoo-48885.upstash.io/get/user_1_session", {
+            headers: {
+              Authorization: "Bearer Ab71AAIjcDE5ZDk0NTQ5MWY4ZGQ0Y2IxYTdkZTc5MGZkODIzZTQzZHAxMA"
+            }
+          }).then(response => response.json())
+            .then(data => console.log(data));
+
+    },[])
     return <div>
+        <button onClick={handleClick}>set</button>
+        <button onClick={handleGet}>get</button>
 
 <Link href="/
 ">Dashboard</Link>
